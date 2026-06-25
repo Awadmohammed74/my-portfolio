844bfd1fb6da5abab54ae8406b3cf7f2706926ec
Awad <awadmhmd666@gmail.com>
2026-06-25 20:33:46 +0300

    padding

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,7 @@ export default function Hero() {
   return (
     <section
       id="home"
-      className="relative min-h-screen flex items-center justify-center overflow-hidden pt-20"
+      className="relative min-h-screen flex items-center justify-center overflow-hidden pt-20 pb-5"
     >
       <div
         className="absolute inset-0"
